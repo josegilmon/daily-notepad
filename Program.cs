f0cabce4a5d5137053a58669c1015d421c326adc
Jose Antonio Gil <joseantonio.gil@intelygenz.com>
2017-09-22 14:37:37 +0200

    Create .NET core project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace daily_notepad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
